25bcc6a0e12c9cfc201d9a13482fa2ae1fda2c3e
Patipat C <pcbimon@gmail.com>
2019-08-15 13:26:25 +0700

    change typescript variable style

--- index.ts
+++ index.ts
@@ -3,10 +3,10 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
+app.get('/', function (req:any, res:any) {
     res.send('<ul><li>Hello World</li><li><a href="/docx">Example Docx</a></li></ul>');
 });
-app.get('/docx', function (req, res) {
+app.get('/docx', function (req:any, res:any) {
     // Create document
     const doc = new Document();
     // Documents contain sections, you can have multiple sections per document, go here to learn more about sections
