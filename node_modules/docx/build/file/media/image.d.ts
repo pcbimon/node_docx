476ae17aa791065461a2a2891ede7b6ee70a1683
Patipat C <pcbimon@gmail.com>
2019-08-15 12:30:39 +0700

    remove node_modules and add gitignore file

--- node_modules/docx/build/file/media/image.d.ts
+++
@@ -1,7 +0,0 @@
-import { ImageParagraph, PictureRun } from "../paragraph";
-export declare class Image {
-    private readonly paragraph;
-    constructor(paragraph: ImageParagraph);
-    readonly Paragraph: ImageParagraph;
-    readonly Run: PictureRun;
-}
--- node_modules/docx/build/file/paragraph/image.d.ts
+++
@@ -1,9 +0,0 @@
-import { IDrawingOptions } from "../drawing";
-import { IMediaData } from "../media";
-import { Paragraph } from "./paragraph";
-import { PictureRun } from "./run";
-export declare class ImageParagraph extends Paragraph {
-    private readonly pictureRun;
-    constructor(imageData: IMediaData, drawingOptions?: IDrawingOptions);
-    readonly Run: PictureRun;
-}
